5ef8da6ed6a4cb03c35e519a70f2541cae4cecf3
Cesar <ccirillobruneri@gmail.com>
2023-05-20 13:55:47 -0300

    feat: change path

---
+++ MANAGER.Backend.Core/Domain/Entities/Users/User.cs
@@ -0,0 +1,14 @@
+﻿using MANAGER.Backend.Core.Domain.Entities.Base;
+
+namespace MANAGER.Backend.Core.Domain.Entities.Users;
+
+public class User : EntityBase
+{
+    public required string Name { get; set; }
+
+    public required string LastName { get; set; }
+
+    public required string Email { get; set; }
+
+    public int Age { get; set; }
+}
---
+++ MANAGER.Backend.Core/Extensions/ObjectExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MANAGER.Backend.Core.Extensions;
+
+public static class ObjectExtensions
+{
+    public static List<T> AsList<T>(this T obj)
+    {
+        if (obj is List<T> list)
+        {
+            return list;
+        }
+        else
+        {
+            return new List<T> { obj };
+        }
+    }
+}
