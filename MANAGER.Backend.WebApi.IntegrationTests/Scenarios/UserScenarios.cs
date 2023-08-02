fabd9b83304b36ca1ba883ce68ef0fcb03285170
Cesar <ccirillobruneri@gmail.com>
2023-08-02 10:56:20 -0300

    #6 - Add tests to Unauthorized and Forbidden

--- MANAGER.Backend.WebApi.IntegrationTests/Scenarios/UserScenarios.cs
+++ MANAGER.Backend.WebApi.IntegrationTests/Scenarios/UserScenarios.cs
@@ -5,6 +5,7 @@ using MANAGER.Backend.WebApi.IntegrationTests.Helper;
 using MANAGER.Backend.WebApi.IntegrationTests.Infrastructure;
 using MANAGER.Backend.WebApi.Model;
 using System.Net;
+using System.Net.Http.Headers;
 using Xunit;
 
 namespace MANAGER.Backend.WebApi.IntegrationTests.Scenarios;
@@ -13,6 +14,82 @@ public class UserScenarios : BaseFixture
 {
     public UserScenarios(CustomWebApplicationFactory<Startup> factory)
     : base(factory) { }
+
+    [Fact]
+    public async Task Post_UserHasNoPermission_ReturnError()
+    {
+        // Arrange
+        var userEmployee = $"UserEmployee_{Guid.NewGuid()}";
+        var userName = $"NewUserName_{Guid.NewGuid()}";
+        var permissions = new List<Roles> { Roles.Employee };
+
+        var employee = new User
+        (
+            userEmployee,
+            "name",
+            $"{userEmployee}@test.com",
+            "password",
+            permissions
+        );
+
+        var userInput = new UserInput
+        {
+            Name = userName,
+            Email = $"{userName}@test.com",
+            LastName = "name",
+            Password = "password",
+            Permissions = permissions
+        };
+
+        await TestManagerContext.Users.AddAsync(employee);
+        await TestManagerContext.SaveChangesAsync();
+
+        var login = new LoginInput
+        {
+            Email = employee.Email,
+            Password = employee.Password,
+        };
+
+        HttpResponseMessage tokenResponse = await _client
+            .PostAsync("api/authentication", GenerateRequestContent(login));
+
+        var token = await tokenResponse.Content.ReadAsStringAsync();
+
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+        // Act
+        var response = await _client
+            .PostAsync("api/user/create-user", GenerateRequestContent(userInput));
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+    }
+
+    [Fact]
+    public async Task Post_UserUnauthorized_ReturnError()
+    {
+        // Arrange
+        var userName = $"NewUserName_{Guid.NewGuid()}";
+        var permissions = new List<Roles> { Roles.Employee };
+
+        var userInput = new UserInput
+        {
+            Name = userName,
+            Email = $"{userName}@test.com",
+            LastName = "name",
+            Password = "password",
+            Permissions = permissions
+        };
+
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer");
+
+        // Act
+        var response = await _client
+            .PostAsync("api/user/create-user", GenerateRequestContent(userInput));
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+    }
 
     [Fact]
     public async Task Post_NewUser_ReturnOk()
@@ -81,7 +158,7 @@ public class UserScenarios : BaseFixture
     }
 
     [Fact]
-    public async Task Post_GetAllUsers_ReturnUsers()
+    public async Task Get_GetAllUsers_ReturnUsers()
     {
         // Arrange
         var userNameOne = $"NewUserName_{Guid.NewGuid()}";
@@ -121,5 +198,113 @@ public class UserScenarios : BaseFixture
         var result = await GetDeserealizeContent<List<User>>(response.Content);
         result.Should().NotBeNull();
         result.Should().Contain(users);
+    }
+
+    [Fact]
+    public async Task Get_GetAllUsersWhenUserHasNoPermission_ReturnError()
+    {
+        // Arrange
+        var userNameOne = $"NewUserName_{Guid.NewGuid()}";
+        var userNameTwo = $"NewUserName_{Guid.NewGuid()}";
+        var userEmployee = $"UserEmployee_{Guid.NewGuid()}";
+
+        var permissions = new List<Roles> { Roles.Admin };
+        var permissionEmployee = new List<Roles> { Roles.Employee };
+
+        var employee = new User
+        (
+            userEmployee,
+            "name",
+            $"{userEmployee}@test.com",
+            "password",
+            permissionEmployee
+        );
+
+        var users = new List<User>
+        {
+            new User
+            (
+                userNameOne,
+                $"{userNameOne}@test.com",
+                "cesar",
+                "password",
+                permissions
+            ),
+            new User
+            (
+                userNameTwo,
+                $"{userNameTwo}@test.com",
+                "bruneri",
+                "password",
+                permissions
+            ),
+            employee
+        };
+
+        var login = new LoginInput
+        {
+            Email = employee.Email,
+            Password = employee.Password,
+        };
+
+        await TestManagerContext.AddRangeAsync(users);
+        TestManagerContext.SaveChanges();
+
+        HttpResponseMessage tokenResponse = await _client
+            .PostAsync("api/authentication", GenerateRequestContent(login));
+
+        var token = await tokenResponse.Content.ReadAsStringAsync();
+
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+        // Act
+        HttpResponseMessage response = await _client
+            .GetAsync("api/user/all-users");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+    }
+
+    [Fact]
+    public async Task Get_GetAllUsersWhenUserUnauthorized_ReturnError()
+    {
+        // Arrange
+        var userNameOne = $"NewUserName_{Guid.NewGuid()}";
+        var userNameTwo = $"NewUserName_{Guid.NewGuid()}";
+
+        var permissions = new List<Roles> { Roles.Admin };
+
+
+        var users = new List<User>
+        {
+            new User
+            (
+                userNameOne,
+                $"{userNameOne}@test.com",
+                "cesar",
+                "password",
+                permissions
+            ),
+            new User
+            (
+                userNameTwo,
+                $"{userNameTwo}@test.com",
+                "bruneri",
+                "password",
+                permissions
+            ),
+        };
+
+        await TestManagerContext.AddRangeAsync(users);
+        TestManagerContext.SaveChanges();
+
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer");
+
+        // Act
+        HttpResponseMessage response = await _client
+            .GetAsync("api/user/all-users");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
     }
 }
